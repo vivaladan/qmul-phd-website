abfbf642e36e7ff212deb91ed9b7817e6544a974
Daniel Revell <daniel.revell@navico.com>
2020-05-24 11:15:43 +0100

    fix function

--- api/AddSignature/index.js
+++ api/AddSignature/index.js
@@ -1,13 +1,13 @@
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = async function (context, req) {
-    context.bindings.signature = JSON.stringify({
-        id: uuidv4(),
-        timestamp: new Date(),
-        name: req.body.name,
-        institution: req.body.institution,
-        email: req.body.email,
-    })
+    // context.bindings.signature = JSON.stringify({
+    //     id: uuidv4(),
+    //     timestamp: new Date(),
+    //     name: req.body.name,
+    //     institution: req.body.institution,
+    //     email: req.body.email,
+    // })
     context.res = {
         body: {}
     };
